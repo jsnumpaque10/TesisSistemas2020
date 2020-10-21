9544e32a8db94f4aa98fd5ca60300ff63d11ad25
jsnumpaque10 <js.numpaque10@uniandes.edu.co>
2020-10-21 18:23:42 -0500

    DAG generator implemented

--- TesisSistemas/src/EstructurasDeDatos/DagGenerator.java
+++ TesisSistemas/src/EstructurasDeDatos/DagGenerator.java
@@ -1,5 +1,7 @@
 package EstructurasDeDatos;
 
+import edu.princeton.cs.algs4.Digraph;
+
 //Generates a random DAG that has the number of vertices given
 public class DagGenerator {
 	
@@ -16,9 +18,58 @@ public class DagGenerator {
 		adjacencyMatrizDAG = new int [V][V];
 	}
 	
-	public int[][] generateAdjacencyMatrixDAG()
+	/**
+	 * Generates the adjacency matrix of a DAG with V vertices
+	 * @return The adjacency matrix of the DAG
+	 */
+	private int[][] generateAdjacencyMatrixDAG()
 	{
-		
+		for (int i = 0; i < V-1 ; i++)
+		{
+			for (int j = 0; j < V ; j++)
+			{
+				if (j >= i )
+				{
+					adjacencyMatrizDAG[i][j]=0;
+				}
+				else
+				{
+					double temp = Math.random();
+					if (temp<=0.5)
+					{
+						adjacencyMatrizDAG[i][j]= 0;
+					}
+					else{
+						adjacencyMatrizDAG[i][j]= 1;
+					}
+
+				}
+			}
+		}
 		return adjacencyMatrizDAG;
+	}
+	
+	/**
+	 * Generates a DAG with V vertices
+	 * @return The DAG with V vertices 
+	 */
+	public Digraph generateDAG()
+	{
+		this.generateAdjacencyMatrixDAG();
+		System.out.println("DAG sucessfully generated with adjacency matrix:");
+		Digraph G = new Digraph(V);
+		for (int i = 0 ; i < V-1 ; i++)
+		{
+			for (int j = 0 ; j < V-1 ; j++)
+			{
+				System.out.print(adjacencyMatrizDAG[i][j]);
+				if (adjacencyMatrizDAG[i][j] == 1)
+				{
+					G.addEdge(i, j);
+				}
+			}
+			System.out.println("");
+		}
+		return G;
 	}
 }
