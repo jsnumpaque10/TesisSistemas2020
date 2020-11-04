31e62f0b89083809a61140daa0e10209fe3fe7d9
jsnumpaque10 <js.numpaque10@uniandes.edu.co>
2020-11-04 11:25:50 -0500

    Se comienza a implementar LIFO

--- TesisSistemasLIFO/src/EstructurasDeDatosLIFO/ControllerLIFO.java
+++ TesisSistemasLIFO/src/EstructurasDeDatosLIFO/ControllerLIFO.java
@@ -1,5 +1,64 @@
 package EstructurasDeDatosLIFO;
 
+import java.util.ArrayList;
+
+
+import edu.princeton.cs.algs4.Digraph;
+
 public class ControllerLIFO {
+	
+	// The multithreaded computation
+	
+	MultithreadedComputationLIFO multithreadedComputation;
+	
+	// Work stealing controller
+	ControllerStealingLIFO workStealingController;
+	
+	// Arraylist of processors
+	ArrayList<ProcesadorLIFO> processors;
+	
+	public ControllerLIFO (Digraph pG, int numberOfProcessors)
+	{
+		multithreadedComputation = new MultithreadedComputationLIFO(pG,numberOfProcessors);
+		workStealingController = new ControllerStealingLIFO(multithreadedComputation);
+
+		//Create and store the processors
+		for (int i = 0 ; i < numberOfProcessors ; i++)
+		{
+			workStealingController.addProcessor(new ProcesadorLIFO(multithreadedComputation,i));
+			
+			System.out.println("Processor with id " + i + " has been created.");
+		}
+		
+		processors = workStealingController.getProcessors();
+		
+	}
+	
+	// Beginning of the execution
+	
+	public void startExecution()
+	{	
+		// Starts each one of the processors
+		for ( int i = 0 ; i < processors.size(); i++)
+		{
+			if(i == 0)
+			{
+				//Notifies that each processor has started its execution
+				System.out.println("Processor " + i + " has started the execution.");
+				// Starts the work stealing controller
+				System.out.println("Work stealing controller has started execution. ");
+				workStealingController.start();
+				processors.get(i).start();
+			}
+			else
+			{
+				//Notifies that each processor has started its execution
+				System.out.println("Processor " + i + " has started the execution.");
+				processors.get(i).start();
+			}
+
+		}
+		
+	}
 
 }
--- TesisSistemasLIFO/src/EstructurasDeDatosLIFO/ControllerStealingLIFO.java
+++ TesisSistemasLIFO/src/EstructurasDeDatosLIFO/ControllerStealingLIFO.java
@@ -1,5 +1,57 @@
 package EstructurasDeDatosLIFO;
 
-public class ControllerStealingLIFO {
+import java.util.ArrayList;
+
+public class ControllerStealingLIFO extends Thread {
+	
+	
+	// Array that stores the processors
+	
+	ArrayList<ProcesadorLIFO> processors;
+	
+	// Computation that is going to be processed 
+	MultithreadedComputationLIFO computation;
+	
+	
+	public ControllerStealingLIFO(MultithreadedComputationLIFO pComputacion)
+	{
+		processors = new ArrayList<ProcesadorLIFO>();
+		computation = pComputacion;
+	}
+	
+	/**
+	 * Add a processor to the work stealing controller
+	 * @param procesador The processor that wants to be added
+	 */
+	public void addProcessor(ProcesadorLIFO procesador)
+	{
+		processors.add(procesador);
+	}
+	
+	public void run()
+	{	
+		while(computation.numberOfVisitedVertices()!= computation.getNumberVerticesG())
+		{
+			for (int i=0 ; i< processors.size(); i++)
+			{
+				if (processors.get(i).getIsStealing() == false && processors.get(i).setVertexToSteal())
+				{
+					try {
+						sleep(300);
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+					break;
+				}
+			}
+		}
+	}
+	
+	// Get methods
+	
+	public ArrayList<ProcesadorLIFO> getProcessors()
+	{
+		return processors;
+	}
 
 }
